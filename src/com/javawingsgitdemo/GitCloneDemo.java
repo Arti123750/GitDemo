e6f2df75f3d98e6dc3acdb811a79a05fd2d991d0
Arti123750 <bombalearti@gmail.com>
2019-10-15 11:16:12 +0530

    Added Prject

---
+++ src/com/javawingsgitdemo/GitCloneDemo.java
@@ -0,0 +1,13 @@
+package com.javawingsgitdemo;
+
+public class GitCloneDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hiii......");
+		System.out.println("www.git demo.com");
+		
+	}
+
+}
